7a624904e7a864d07cd86ea4535875aa5a6b7458
Aliaksei Korneev <aliaksei_korneev@epam.com>
2016-04-02 20:29:06 +0300

    adding jmx host property

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,8 +4,9 @@ default['mysql']['server_repl_password'] = '11111'
 default['pulse']['source_location'] = '/opt/pulse/source/'
 default['pulse']['user'] = 'pulse'
 default['pulse']['group'] = 'pulse'
+default['pulse']['jmx_host'] = node['ipaddress']
 default['pulse']['build_location'] = '/usr/share/tomcat6/webapps/'
-default['pulse']['rmi_opts'] = ' -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false'
+default['pulse']['rmi_opts'] = " -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false  -Djava.net.preferIPv4Stack=true -Djava.rmi.server.hostname=#{node['pulse']['jmx_host']}"
 default['pulse']['db_user'] = 'pulse'
 default['pulse']['db_pass'] = 'pulse'
 default['pulse']['db_name'] = 'pulse'
